2d5737398efbb946e126a1bf207c026306b6f37d
bang-star <bang77060986@gmail.com>
2023-02-02 16:59:50 +0900

    [반복문] for문과 향상된 for문

---
+++ src/ch04/iterations/FloxEx14.java
@@ -0,0 +1,11 @@
+package ch04.iterations;
+
+public class FloxEx14 {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int i = 1, j = 10; i <= 10; i++, j--) {
+            System.out.printf("%d \t %d\n", i, j);
+        }
+    }
+}
