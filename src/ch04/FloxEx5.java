9b82e324c8b0266f130e965a5b69ee7638042318
bang-star <bang77060986@gmail.com>
2023-02-02 16:07:03 +0900

    [조건문] if

---
+++ src/ch04/FloxEx5.java
@@ -0,0 +1,38 @@
+package ch04;
+
+import java.util.Scanner;
+
+public class FloxEx5 {
+    public static void main(String[] args) {
+        char grade = ' ';
+        char opt = '0';
+
+        System.out.print("점수를 입력하세요. >");
+
+        Scanner scanner = new Scanner(System.in);
+        String tmp = scanner.nextLine();
+        int score = Integer.parseInt(tmp);
+
+        System.out.printf("점수 : %d.\n", score);
+
+        if (score >= 90) {
+            grade = 'A';
+            if (score >= 98) {
+                opt = '+';
+            } else if (score < 94) {
+                opt = '-';
+            }
+        } else if (score >= 80) {
+            grade = 'B';
+            if (score >= 88) {
+                opt = '+';
+            } else if (score < 84) {
+                opt = '-';
+            }
+        } else {
+            grade = 'C';
+        }
+
+        System.out.printf("학점은 %c%c 입니다.%n", grade, opt);
+    }
+}
