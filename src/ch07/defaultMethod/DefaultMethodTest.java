577155ffd16ca2394bc5a67872c12e7f90ee5b0c
bang-star <bang77060986@gmail.com>
2023-02-08 03:05:43 +0900

    [인터페이스] 디폴트 메서드와 static 메서드
    JDK 1.8부터 인터페이스에 디폴트 메서드와 static 메서드도 추가할 수 있게 되었다. 
    
    주의사항
    1. 여러 인터페이스의 디폴트 메서드 간의 충돌
    2. 디폴트 메서드와 조상 클래스의 메서드 간의 충돌

---
+++ src/ch07/defaultMethod/DefaultMethodTest.java
@@ -0,0 +1,12 @@
+package ch07.defaultMethod;
+
+public class DefaultMethodTest {
+    public static void main(String[] args) {
+        Child c = new Child();
+        c.method1();
+        c.method2();
+
+        MyInterface.staticMethod();
+        MyInterface2.staticMethod();
+    }
+}
---
+++ src/ch07/defaultMethod/Parent.java
@@ -0,0 +1,7 @@
+package ch07.defaultMethod;
+
+public class Parent {
+    public void method2() {
+        System.out.println("method2() in Parent");
+    }
+}
